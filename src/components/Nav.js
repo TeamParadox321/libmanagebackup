e3f0dd28e50100074928142cc5534049aec32f07
Sandeep Kushaj Vithanage <sandeepkushaj3@gmail.com>
2019-12-06 20:49:50 +0530

    app.js edited

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,33 +4,34 @@ import Logo from "./uoklogo.png";
 
 function Nav(){
     return (
-        <nav className="navb navbar navbar-expand-md navbar-dark bg-dark">
-            <Link to={"/"} className="navbar-brand">
-                <img src={Logo} width={"45"} height={"45"} alt="CoolBrand" />
-            </Link>
-            <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                <span className="navbar-toggler-icon"></span>
-            </button>
+        <div>
+            <nav className="navb navbar navbar-expand-md navbar-dark bg-dark">
+                <Link to={"/"} className="navbar-brand">
+                    <img src={Logo} width={"45"} height={"45"} alt="CoolBrand" />
 
-            <div className="collapse navbar-collapse" id="navbarCollapse">
-                <div className="navbar-nav">
-                    <Link to={"/"}> <h2 className={"header-text"}>Library Management System </h2> </Link>
-                    <Link to={"/"} className="nav-item nav-link"> Home </Link>
-                    <Link to="/" className="nav-item nav-link"> Books </Link>
-                    <Link to="/" className="nav-item nav-link"> AAAAAA </Link>
-                    <Link to={"/"} className="nav-item nav-link disabled" tabIndex="-1"> BBBB </Link>
+                </Link>
+                <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarCollapse">
+                    <div className="navbar-nav">
+                        <Link to={"/"}> <h4 className={"header-text"}>Library Management System </h4> </Link>
+                    </div>
+                    <div className="navbar-nav">
+                        <Link to={"/"} className="nav-item nav-link"> Home </Link>
+                        <Link to="/" className="nav-item nav-link"> Books </Link>
+                        <Link to="/" className="nav-item nav-link"> AAAAAA </Link>
+                        <Link to={"/"} className="nav-item nav-link disabled" tabIndex="-1"> BBBB </Link>
+                    </div>
+                    <div className="navbar-nav ml-auto">
+                        <Link to={"/"} className="nav-item nav-link"> Sign Up </Link>
+                        <Link to={"/student_login"} className="nav-item nav-link"> Student Login </Link>
+                        <Link to={"/admin_login"} className="nav-item nav-link">Admin Login</Link>
+                    </div>
                 </div>
-                <div className="navbar-nav ml-auto">
-                    <Link to={"/"} className="nav-item nav-link"> Sign Up </Link>
-                    <Link to={"/student_login"} className="nav-item nav-link"> Student Login </Link>
-                    <Link to={"/admin_login"} className="nav-item nav-link">Admin Login</Link>
-                </div>
-
-
-
-            </div>
-        </nav>
-
+            </nav>
+        </div>
     );
 }
 
