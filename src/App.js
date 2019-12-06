fe8ca93ac31db7a8aeedec46ebd6b897b8c79956
navaNoobCoder <nkindelpitiya@gmail.com>
2019-12-06 21:19:17 +0530

    signup added by nava

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import Home from './components/Home';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
+import AdminLogin from "./components/AdminLogin";
 function App() {
     return (
         <Router>
             <Header/>
             <div className={"section"}>
                 <Route path="/" exact component={Home} />
-                <Route path="/student_login" exact component={Login} />
-                <Route path="/admin_login" component={Login}/>
+                <Route path="/student_login" component={Login} />
+                <Route path="/admin_login" component={AdminLogin}/>
+                <Route path="/signup" component={Signup}/>
             </div>
             <Footer />
         </Router>
