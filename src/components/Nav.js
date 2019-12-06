3e896c73aaccec510567efdcc6ecd700cf747c03
Sandeep Kushaj Vithanage <sandeepkushaj3@gmail.com>
2019-12-06 14:43:39 +0530

    app.js edited

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,35 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from 'react'
+import { Link } from "react-router-dom";
+import Logo from "./uoklogo.png";
 
 function Nav(){
     return (
-        <nav className="navbar navbar-inverse">
-            <div className="container-fluid">
-                <div className="navbar-header">
-                    <Link className="navbar-brand" to="#">FCT LIBRARY MANAGEMENT SYSTEM</Link>
+        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+            <a href="#" className="navbar-brand">
+                <img src={Logo} width={"28"} height="28" alt="CoolBrand" />
+            </a>
+            <button type="button" className="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarCollapse">
+                <div className="navbar-nav">
+                    <a href="#" className="nav-item nav-link active">Home</a>
+                    <a href="#" className="nav-item nav-link">Books</a>
+                    <a href="#" className="nav-item nav-link"> AAAAAA </a>
+                    <a href="#" className="nav-item nav-link disabled" tabIndex="-1"> BBBB </a>
                 </div>
-                <ul className="nav navbar-nav">
-                    <li className="active"><Link to="#">Home</Link></li>
-                    <li><Link to="#">Books</Link></li>
-                    <li><Link to="#">History</Link></li>
-                </ul>
-                <ul className="nav navbar-nav navbar-right">
-                    <li><Link to="/"><span className="glyphicon glyphicon-user"></span> Sign Up</Link></li>
-                    <li><Link to="/student_login"><span className="glyphicon glyphicon-log-in"></span> User Login</Link></li>
-                    <li><Link to="/admin_login"><span className="glyphicon glyphicon-log-in"></span> Admin Login</Link></li>
-                </ul>
+                <div className="navbar-nav ml-auto">
+                    <a href="#" className="nav-item nav-link"> Sign Up </a>
+                    <a href="#" className="nav-item nav-link"> Student Login </a>
+                    <a href="#" className="nav-item nav-link">Admin Login</a>
+                </div>
+
+
+
             </div>
         </nav>
+
     );
 }
 
