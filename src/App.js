5775b9700e34f6dbdd67ebffdb0597fa33a74638
navaNoobCoder <nkindelpitiya@gmail.com>
2019-12-07 22:31:32 +0530

    Admin Dashboard added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./components/style.css"
+import "./components/Dashboards/simple-sidebar.css"
 import './App.css';
 import Home from './components/Home';
 import Header from "./components/Header";
@@ -8,6 +9,7 @@ import Footer from "./components/Footer";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import AdminLogin from "./components/AdminLogin";
+import AdminDashboard from "./components/Dashboards/AdminDashboard";
 function App() {
     return (
         <Router>
@@ -17,8 +19,9 @@ function App() {
                 <Route path="/student_login" component={Login} />
                 <Route path="/admin_login" component={AdminLogin}/>
                 <Route path="/student_signup" component={Signup}/>
+                <Route path="/admin_dashboard" component={AdminDashboard}/>
             </div>
-            <Footer />
+            <Footer/>
         </Router>
     );
 }
