a4f96d0e8a6fd83bdfd2d0317dd39257174c1071
Sandeep Kushaj Vithanage <sandeepkushaj3@gmail.com>
2019-12-08 08:56:18 +0530

    app.js edited

--- src/components/Dashboards/AdminDashboard.js
+++ src/components/Dashboards/AdminDashboard.js
@@ -7,7 +7,7 @@ function AdminDashboard() {
 
         <div className="d-flex" id="wrapper">
             <div className="bg-light border-right" id="sidebar-wrapper">
-                <div className="sidebar-heading">Welcome Admin!</div>
+                <div className="sidebar-heading">Admin Dashboard</div>
                 <div className="list-group list-group-flush">
                     <a href="#" className="list-group-item list-group-item-action bg-light">Dashboard</a>
                     <a href="#" className="list-group-item list-group-item-action bg-light">Manage Users</a>
@@ -26,23 +26,7 @@ function AdminDashboard() {
 
                     <div class="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
-                            <li class="nav-item active">
-                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Link</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    Dropdown
-                                </a>
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="#">Action</a>
-                                    <a class="dropdown-item" href="#">Another action</a>
-                                    <div class="dropdown-divider"></div>
-                                    <a class="dropdown-item" href="#">Something else here</a>
-                                </div>
-                            </li>
+
                         </ul>
                     </div>
                 </nav>
