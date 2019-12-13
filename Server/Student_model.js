f341eade6f3f2018fb459b64a249afa9cf48abd7
navaNoobCoder <nkindelpitiya@gmail.com>
2019-12-13 15:40:14 +0530

    user registration updated

---
+++ Server/Student_model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Student = new Schema({
+    stu_id: {
+        type: String
+    },
+    stu_email: {
+        type: String
+    },
+    stu_name: {
+        type: String
+    },
+    stu_password: {
+        type: String
+    },
+});
+
+module.exports = mongoose.model('Student', Student);
