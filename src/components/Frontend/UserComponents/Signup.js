1ab3158c05607888a490cc47943d09eab89c101e
navaNoobCoder <nkindelpitiya@gmail.com>
2019-12-13 11:55:43 +0530

    user registration added

--- src/components/Frontend/UserComponents/Signup.js
+++ src/components/Frontend/UserComponents/Signup.js
@@ -1,32 +1,46 @@
-import React from 'react'
+import React,{Component} from 'react';
+import axios from 'axios';
 
+class Signup extends Component() {
+    constructor(props) {
+        super(props);
+        this.state={
+            stu_id:"",
+            stu_email:"",
+            stu_name:"",
+            stu_password:"",
+            stu_confirm_password:""
+        }
+    }
+    render() {
+        return(
+            <div className="login-form">
+                <form>
+                    <h2 className="text-center">Sign up</h2>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Student ID" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Email" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="text" className="form-control" placeholder="Name With Initials" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control" placeholder="Password" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <input type="password" className="form-control" placeholder="Confirm Password" required="required"/>
+                    </div>
+                    <div className="form-group">
+                        <button type="submit" className="btn btn-primary btn-block">Sign up</button>
+                    </div>
+                </form>
+            </div>
+        )
 
-function Signup() {
-    return(
-        <div className="login-form">
-            <form>
-                <h2 className="text-center">Sign up</h2>
-                <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Student ID" required="required"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Email" required="required"/>
-                </div>
-                <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Name With Initials" required="required"/>
-                </div>
-                <div className="form-group">
-                    <input type="password" className="form-control" placeholder="Password" required="required"/>
-                </div>
-                <div className="form-group">
-                    <input type="password" className="form-control" placeholder="Confirm Password" required="required"/>
-                </div>
-                <div className="form-group">
-                    <button type="submit" className="btn btn-primary btn-block">Sign up</button>
-                </div>
-            </form>
-        </div>
-    )
+    }
+
 
 }
 
